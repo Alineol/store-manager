a768e6988ca65753c19bbb9ba9d4c111946f2694
Aline <allinne.oliveira.ol@gmail.com>
2022-05-25 20:58:45 -0300

    feat: cria rota sale e sales/id

--- routes.js
+++ routes.js
@@ -3,10 +3,15 @@ const express = require('express');
 const route = express.Router();
 
 const productsController = require('./controllers/productsController');
+const salesController = require('./controllers/salesController');
 
 route.get('/products/:id', productsController.getById);
 
 route.get('/products', productsController.getAll);
+
+route.get('/sales/:id', salesController.getById);
+
+route.get('/sales', salesController.getAll);
 
 // route.get('sales',)
 
