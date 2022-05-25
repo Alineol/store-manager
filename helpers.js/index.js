a1fb368ecc7c8d4e9a55579c54c940f69d5ce5d7
Aline <allinne.oliveira.ol@gmail.com>
2022-05-25 19:57:42 -0300

    fix: corrige retorno de checkLenght

--- helpers.js/index.js
+++ helpers.js/index.js
@@ -1,9 +1,17 @@
 const checkLenght = (array, name) => {
 if (array.lenght === 0) {
-  return { s: 200, m: `Não existem ${name} cadastrado(s)` };
+  return { s: 404, message: `Não existem ${name} cadastrado(s)` };
 } 
-return { s: 200, m: array };
+return { s: 200, message: array };
 };
+
+// const checkId = (result, name) => {
+//   if (result[0]) {
+//     const response = result[0];
+//     return { s: 200, response };
+//   }
+//   return { s: 200, message: `${name} not found` };
+// };
 
 module.exports = {
   checkLenght,
