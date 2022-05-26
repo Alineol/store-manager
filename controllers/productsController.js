ccedbe8657d97991c245844fa68d0edade02e811
Aline <allinne.oliveira.ol@gmail.com>
2022-05-26 14:17:40 -0300

    improve: adiciona função de middleware

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,17 +3,17 @@ const productsService = require('../services/productsService');
 const getAll = async (_req, res) => {
   const products = await productsService
     .getAll();
-    const { s, message } = products;
-  return res.status(s).json(message);
+ if (products.message) { return res.status(products.code).json({ message: products.message }); }
+ return res.status(200).json(products);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
   const product = await productsService.getById(id);
-  if (product[0]) {
-    return res.status(200).json(product[0]);
+  if (product.message) {
+    return res.status(product.code).json({ message: product.message });
   }
-    return res.status(404).json({ message: 'Product not found' });
+  return res.status(200).json(product[0]);
 };
 
 module.exports = {
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -4,17 +4,15 @@ const salessService = require('../services/salesService');
 const getAll = async (_req, res) => {
   const sales = await salessService
     .getAll();
-    const { s, message } = sales;
-  return res.status(s).json(message);
+  if (sales.message) { return res.status(sales.code).json({ message: sales.message }); }
+  return res.status(200).json(sales);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
   const sale = await salessService.getById(id);
-  if (sale.length > 0) {
-    return res.status(200).json(sale);
-  }
-    return res.status(404).json({ message: 'Sale not found' });
+  if (sale.message) { return res.status(sale.code).json({ message: sale.message }); }
+  return res.status(200).json(sale);
 };
 
 module.exports = {
