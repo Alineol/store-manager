5de3eafc01b83048708633c5a96f8a54bd7cd752
Aline <allinne.oliveira.ol@gmail.com>
2022-05-27 19:37:20 -0300

    test: cria  edit, delete e create na camada products/model

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -72,3 +72,49 @@ describe('busca produto pelo id(model)', () => {
     })
   })
 })
+
+describe('Ao editar um produto com sucesso:', () => {
+  before(() => {
+    const resultExecute = [[{affectedRows: 1}]];
+
+    sinon.stub(connection, 'execute').resolves(resultExecute)
+  });
+  after(() => {
+    connection.execute.restore();
+  })
+  it('retorna um array não vazio', async() => {
+    const response = await productsModel.edit(1, 'name', 1);
+    expect(response).to.be.an('array');
+    expect(response).to.not.be.empty;
+  })
+})
+
+describe('Ao deletar um produto com sucesso:', () => {
+  before(() => {
+    const resultExecute = [[{affectedRows: 1}]];
+
+    sinon.stub(connection, 'execute').resolves(resultExecute)
+  });
+  after(() => {
+    connection.execute.restore();
+  })
+  it('retorna um array não vazio', async() => {
+    const response = await productsModel.deleteProduct(1);
+    expect(response).to.be.an('array');
+    expect(response).to.not.be.empty;
+  })
+})
+
+describe('Ao criar um produto com sucesso:', () => {
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([{insertId: 1}])
+  });
+  after(() => {
+    connection.execute.restore();
+  })
+  it('retorna o numero do novo id criado', async() => {
+    const response = await productsModel.create('name', 2);
+    expect(response).to.be.an('number');
+    // expect(response).to.not.be.empty;
+  })
+})
