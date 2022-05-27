312735feda90e05bb0cd7215f1ea290c2bb6328b
Aline <allinne.oliveira.ol@gmail.com>
2022-05-27 11:31:07 -0300

    feat: função create na camada controller/sales

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,8 +1,8 @@
-const salessService = require('../services/salesService');
+const salesService = require('../services/salesService');
 // const { checkLength } = require('../middlewares/index');
 
 const getAll = async (_req, res) => {
-  const sales = await salessService
+  const sales = await salesService
     .getAll();
   if (sales.message) { return res.status(sales.code).json({ message: sales.message }); }
   return res.status(200).json(sales);
@@ -10,12 +10,20 @@ const getAll = async (_req, res) => {
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const sale = await salessService.getById(id);
+  const sale = await salesService.getById(id);
   if (sale.message) { return res.status(sale.code).json({ message: sale.message }); }
   return res.status(200).json(sale);
+};
+
+const create = async (req, res) => {
+  const sales = req.body;
+  console.log(sales);
+  const response = await salesService.create(sales);
+  res.status(201).json(response);
 };
 
 module.exports = {
   getById,
   getAll,
+  create,
 };
