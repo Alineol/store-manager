e41c0b65092ecd44775e7c97f642b04410cf3241
Aline <allinne.oliveira.ol@gmail.com>
2022-05-30 19:59:04 -0300

    test: create na camada Sale/controller Mudanças a serem submetidas:

--- test/unit/controllers/salesController.js
+++ test/unit/controllers/salesController.js
@@ -2,10 +2,10 @@ const sinon = require('sinon');
 const {expect} = require ('chai');
 const salesController = require('../../../controllers/salesController')
 const salesService = require('../../../services/salesService');
-const response = {}
-const request = {}
 
-describe('Busca todos as vendas no BD(controller)', () => {
+describe('Busca todas as vendas no BD(controller)', () => {
+  const response = {}
+  const request = {}
 
   describe('-quando não existe nenhum produto no BD:', () => {
     before(() => {
@@ -61,6 +61,8 @@ describe('Busca todos as vendas no BD(controller)', () => {
 })
 
 describe(' Busca vendas pelo id(controler)', () => {
+  const response = {}
+  const request = {}
   describe('- quando uma ou mais vendas são encontradas:', () => {
 
     before(() => {
@@ -113,3 +115,159 @@ describe(' Busca vendas pelo id(controler)', () => {
     })
   })
 })
+
+describe('Cria uma nova venda:', () => {
+  const response = {}
+  const request = {}
+  describe('- ao criar venda com um produto', () => {
+
+    before(() => {
+      request.body =  [
+        {
+          "productId": 1,
+          "quantity": 3
+        }
+      ]
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns()
+
+      sinon.stub(salesService, 'create').resolves({ id: 10, itemsSold: [
+        {
+          "productId": 1,
+          "quantity": 3
+        }
+      ] });
+    })
+
+    after(() => {
+      salesService.create.restore()
+    })
+
+    it('é chamado status 201 com um objeto no json', async() => {
+      await salesController.create(request, response);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+    })
+  })
+
+  describe('- ao criar venda com mais de um produto:', () => {
+
+    before(() => {
+      request.body =    [
+        {
+          "productId": 1,
+          "quantity": 2
+        },
+        {
+          "productId": 2,
+          "quantity": 5
+        }
+      ]
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns()
+
+      sinon.stub(salesService, 'create').resolves( {
+        "id": 1,
+        "itemsSold": [
+          {
+            "productId": 1,
+            "quantity": 2
+          },
+          {
+            "productId": 2,
+            "quantity": 5
+          }
+        ]
+      });
+    })
+
+    after(() => {
+      salesService.create.restore()
+    })
+
+    it('é chamado status 201 com um objeto no json', async() => {
+      await salesController.create(request, response);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+    })
+  })
+})
+
+// describe('Edita uma venda:', () => {
+//   const response = {}
+//   const request = {}
+//   describe('- ao registrar uma venda com sucesso:', () => {
+
+//     before(() => {
+//       request.body =  [
+//         {
+//           "productId": 1,
+//           "quantity": 3
+//         }
+//       ]
+//       response.status = sinon.stub().returns(response);
+//       response.json = sinon.stub().returns()
+
+//       sinon.stub(salesService, 'create').resolves({ id: 10, itemsSold: [
+//         {
+//           "productId": 1,
+//           "quantity": 3
+//         }
+//       ] });
+//     })
+
+//     after(() => {
+//       salesService.create.restore()
+//     })
+
+//     it('é chamado status 201 com um objeto no json', async() => {
+//       await salesController.create(request, response);
+//       expect(response.status.calledWith(201)).to.be.equal(true);
+//       expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+//     })
+//   })
+
+//   describe('- ao registrar venda, com mais de um produto ,com sucesso:', () => {
+
+//     before(() => {
+//       request.body =    [
+//         {
+//           "productId": 1,
+//           "quantity": 2
+//         },
+//         {
+//           "productId": 2,
+//           "quantity": 5
+//         }
+//       ]
+//       response.status = sinon.stub().returns(response);
+//       response.json = sinon.stub().returns()
+
+//       sinon.stub(salesService, 'create').resolves( {
+//         "id": 1,
+//         "itemsSold": [
+//           {
+//             "productId": 1,
+//             "quantity": 2
+//           },
+//           {
+//             "productId": 2,
+//             "quantity": 5
+//           }
+//         ]
+//       });
+//     })
+
+//     after(() => {
+//       salesService.create.restore()
+//     })
+
+//     it('é chamado status 201 com um objeto no json', async() => {
+//       await salesController.create(request, response);
+//       expect(response.status.calledWith(201)).to.be.equal(true);
+//       expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+//     })
+//   })
+// })
+
+
