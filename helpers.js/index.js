62cbe36364b8796031e5d634e8f767038da8e391
Aline <allinne.oliveira.ol@gmail.com>
2022-05-31 17:52:07 -0300

    feat: updates products quantity with helpers modified:   helpers.js/index.js

--- helpers.js/index.js
+++ helpers.js/index.js
@@ -20,7 +20,7 @@ const checkId = (result, name) => {
 // };
 // const producstService = require('./productsService');
 
-const checkQuant = async (sales, getById) => sales.map(async (sale) => {
+const checkQuant = async (sales, getById, updateQuantity) => sales.map(async (sale) => {
    const id = sale.productId;
    const saleQuant = sale.quantity;
    const product = await getById(id);
@@ -28,11 +28,23 @@ const checkQuant = async (sales, getById) => sales.map(async (sale) => {
    if (saleQuant > quantity) {
      return { code: 422, message: 'Such amount is not permitted to sell' };
    }
+   const newProductQuant = quantity - saleQuant;
+   await updateQuantity(id, newProductQuant);
    return {};
   });
+
+  const increaseProductQuantity = async (sales, getById, updateQuantity) => {
+    sales.map(async (sale) => {
+      const product = await getById(sale.productId);
+      const { quantity } = product[0];
+      const newProductQuant = quantity + sale.quantity;
+      return updateQuantity(sale.productId, newProductQuant);
+    });
+  };
 
 module.exports = {
    checkLength,
   checkId,
   checkQuant,
+  increaseProductQuantity,
 };
